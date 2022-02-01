9b8f3adb0c7bb81991bfcc59f7ca6c5f3baab6e8
Toni <garciabarreratoni@gmail.com>
2022-02-01 03:36:31 +0100

    gh-pages commit

---
+++ js/chunk-2d0e5e97.610171be.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0e5e97"],{9703:function(n,e,t){"use strict";t.r(e);var c=t("7a23");const o=Object(c["f"])("h1",null,"404 NotFound",-1),u=[o];function d(n,e,t,o,d,r){return Object(c["s"])(),Object(c["e"])("div",null,u)}var r={},s=t("6b0d"),a=t.n(s);const b=a()(r,[["render",d]]);e["default"]=b}}]);
+//# sourceMappingURL=chunk-2d0e5e97.610171be.js.map
